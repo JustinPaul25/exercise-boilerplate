3fe9c1c4ce62286c0fc5c95f8747f0ee06bdcee3
JustinPaul1025 <justinpaul1025@gmail.com>
2021-11-22 14:20:03 +0800

    put in variable

--- src/data/repositories/TodoRepositoryImpl.ts
+++ src/data/repositories/TodoRepositoryImpl.ts
@@ -3,10 +3,12 @@ import TodoRepository from "../../domain/repositories/TodoRepository"
 
 export default class TodoRepositoryImpl implements TodoRepository {
     async GetTodos(): Promise<Todo[]> {
-        return [
+        const todos = [
             {id: 1, title: 'todo1'},
             {id: 2, title: 'todo2'},
             {id: 3, title: 'todo3'}
-        ]
+        ];
+
+        return todos;
     }
 }
