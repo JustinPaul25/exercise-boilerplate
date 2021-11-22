20d58a3f52ffff2e996d57a8de7c3f998ff6e27a
JustinPaul1025 <justinpaul1025@gmail.com>
2021-11-22 14:16:12 +0800

    api to array

--- src/data/repositories/TodoRepositoryImpl.ts
+++ src/data/repositories/TodoRepositoryImpl.ts
@@ -2,11 +2,11 @@ import Todo from "../../domain/entities/Todo"
 import TodoRepository from "../../domain/repositories/TodoRepository"
 
 export default class TodoRepositoryImpl implements TodoRepository {
-    jsonUrl = "https://jsonplaceholder.typicode.com/users/1/todos"
-
     async GetTodos(): Promise<Todo[]> {
-        const res = await fetch(this.jsonUrl)
-        const jsonData = await res.json()
-        return jsonData.map((item: Todo) => ({ id: item.id, title: item.title }))
+        return [
+            {id: 1, title: 'todo1'},
+            {id: 2, title: 'todo2'},
+            {id: 3, title: 'todo3'}
+        ]
     }
 }
