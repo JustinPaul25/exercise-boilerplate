492964b05b36bc981fb9af38a7cad35f6eec7536
Justin Paul Enad Layan <52854317+JustinPaulLayan@users.noreply.github.com>
2021-11-23 09:05:16 +0800

    Update TodoRepository.ts

--- src/domain/repositories/TodoRepository.ts
+++ src/domain/repositories/TodoRepository.ts
@@ -1,5 +1,8 @@
 import Todo from "../entities/Todo"
 
 export default interface TodoRepository {
-    GetTodos(): Promise<Todo[]>
+    GetTodos(): Promise<Todo[]>,
+    StoreTodo(todo, list): Promise<Todo[]>,
+    DeleteTodo(id, list): Promise<Todo[]>,
+    UpdateTodo(item, data, list): Promise<Todo[]>
 }
