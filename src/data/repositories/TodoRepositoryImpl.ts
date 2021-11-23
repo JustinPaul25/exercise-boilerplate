ecd40a2b820608643bba4b847537ccb21d53c118
Justin Paul Enad Layan <52854317+JustinPaulLayan@users.noreply.github.com>
2021-11-23 09:04:26 +0800

    Update TodoRepositoryImpl.ts

--- src/data/repositories/TodoRepositoryImpl.ts
+++ src/data/repositories/TodoRepositoryImpl.ts
@@ -11,4 +11,41 @@ export default class TodoRepositoryImpl implements TodoRepository {
 
         return todos;
     }
+
+    async StoreTodo(data, list): Promise<Todo[]> {
+        const newList = []
+        console.log(data)
+        for(var ctr = 0; ctr < list.length; ctr++) {
+            newList.push({id: list[ctr].id, title: list[ctr].title});
+        }
+        
+        newList.push({id: list[list.length - 1].id + 1, title: data});
+        
+        return newList;
+    }
+
+    async DeleteTodo(id, list): Promise<Todo[]> {
+        const newList = []
+        for(var ctr = 0; ctr < list.length; ctr++) {
+            if(id != list[ctr].id) {
+                newList.push({id: list[ctr].id, title: list[ctr].title});
+            }
+        }
+        
+        return newList;
+    }
+
+    async UpdateTodo(item, data, list): Promise<Todo[]> {
+        const newList = []
+        for(var ctr = 0; ctr < list.length; ctr++) {
+            if(item.id != list[ctr].id) {
+                newList.push({id: list[ctr].id, title: list[ctr].title});
+            } else {
+                newList.push({id: list[ctr].id, title: data});
+            }
+        }
+        
+        return newList;
+    }
 }
+
