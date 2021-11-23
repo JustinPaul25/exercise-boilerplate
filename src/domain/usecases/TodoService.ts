f095290fe7e7bb588d1657034624fe4087c28629
Justin Paul Enad Layan <52854317+JustinPaulLayan@users.noreply.github.com>
2021-11-23 09:07:27 +0800

    Update TodoService.ts

--- src/domain/usecases/TodoService.ts
+++ src/domain/usecases/TodoService.ts
@@ -11,4 +11,16 @@ export default class TodoServiceImpl {
     async GetTodos(): Promise<Todo[]> {
         return this.todoRepo.GetTodos()
     }
+
+    async StoreTodo(data, list): Promise<Todo[]> {
+        return this.todoRepo.StoreTodo(data, list)
+    }
+
+    async DeleteTodo(id, list): Promise<Todo[]> {
+        return this.todoRepo.DeleteTodo(id, list)
+    }
+
+    async UpdateTodo(item, data, list): Promise<Todo[]> {
+        return this.todoRepo.UpdateTodo(item, data, list)
+    }
 }
